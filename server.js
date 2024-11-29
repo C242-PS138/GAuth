6fdf2cb62771e0ddf90bbbcabb0ee929b276f281
syronesdreamland <116932673+syronesdreamland@users.noreply.github.com>
2024-11-30 06:26:03 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const Hapi = require('@hapi/hapi');
 const init = async () => {
     const server = Hapi.server({
         port: 5000,
-        host: process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost'
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     });
 
     server.route({
